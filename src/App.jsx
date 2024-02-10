5ab2227b54849561f9e8063caaf5fe5465f63028
haroldCoder <haroldc2005@gmail.com>
2024-02-10 18:27:18 -0500

    implement button next page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 import Card from './components/Card';
@@ -6,17 +6,18 @@ import Search from './components/Search';
 
 function App() {
   const [rickmorty, setRickmorty] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     getCharacters();
-  }, []);
+  }, [page]);
 
   const getCharacters = async () => {
-    const res = (await axios.get(`https://rickandmortyapi.com/api/character`)).data;
-    console.log(res);
+    let res = (await axios.get(`https://rickandmortyapi.com/api/character?page=${page}`)).data
+    
     setRickmorty(res.results);
   }
-
+  console.log(page);
   return (
     <>
       <div className='p-14'>
@@ -26,10 +27,11 @@ function App() {
         <section className='flex justify-between flex-wrap gap-y-4'>
           {
             rickmorty.map(elemento => (
-              <Card name={elemento.name} id={elemento.id} image={elemento.image} status={elemento.status} species={elemento.species} />
+              <Card name={elemento.name} id={elemento.id} episodes={elemento.episode} image={elemento.image} status={elemento.status} species={elemento.species} />
             ))
           }
         </section>
+        <button onClick={()=>{setPage(page+1)}} className='bg-gradient-to-r mx-[40%] w-[30%] my-12 text-white to-green-600 from-slate-500 rounded-md px-8 p-3'>Next</button>
       </div>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-export default function Card({ id, name, image, status, species }) {
+export default function Card({ id, name, image, status, species, episodes }) {
+    const [episodess, setEpisodes] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        const getEpisodes = () => {
+            let array = []
+            episodes.map(async (e) => {
+                const res = (await axios.get(e)).data;
+                array.push(res)
+            })
+            setEpisodes(array)
+        }
+
+        getEpisodes()
+    }, [])
+
+    console.log(episodess);
+
     return (
         <div className='from-gray-800 to-blue-500 bg-gradient-to-tr rounded-md'>
             <section className='flex justify-between mb-12 bg-gray-800 rounded-t-md p-3'>
@@ -16,6 +35,22 @@ export default function Card({ id, name, image, status, species }) {
 
                 <p className='text-green-500'><b>species: {species}</b></p>
             </section>
+            <section className='p-3 flex justify-center'>
+                <button onClick={()=>setOpen(true)} className=' bg-gradient-to-r text-white to-green-600 from-slate-500 rounded-md px-8 p-3'>Episodios</button>
+            </section>
+            {
+                open ? 
+                <div onDoubleClick={()=>setOpen(false)} className='absolute grid grid-cols-3 gap-5 bg-[#24242444] opacity-90 p-5 w-[60%] top-[20%] left-[30%]' style={{backdropFilter: "blur(6px)"}}>
+                    {
+                        episodess.map((e)=>(
+                            <div className='bg-green-500 rounded-md p-4 px-20'>
+                               <h2 className='text-white'>{e.name}</h2> 
+                            </div>
+                            
+                        ))
+                    }
+                </div> : null
+            }
         </div>
     )
 }
